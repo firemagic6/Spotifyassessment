30ae1560199e5c0259e7f893aefa9c7147f67f78
firemagic6 <yichengstudy@gmail.com>
2024-10-16 16:09:34 +0800

    My spotify assessment

--- src/components/searchbar/SearchBar.jsx
+++ src/components/searchbar/SearchBar.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./SearchBar.css"
-function SearchBar() {
+
+function SearchBar({onSearch, runSearch}) {
+
+  function handleSearchChange(event) { 
+    // call a method to render the search
+    onSearch(event.target.value);
+  }
+
+  function handleSearchClick() {   
+    runSearch();
+  }
+
   return (
     <div className="SearchBar">
-      <input placeholder="Enter A Song, Album, or Artist" />
-      <button className="SearchButton">SEARCH</button>
+      <input placeholder="Enter A Song, Album, or Artist" onChange={handleSearchChange} />
+      <button className="SearchButton" onClick={handleSearchClick}>SEARCH</button>
     </div>
   )
 }
